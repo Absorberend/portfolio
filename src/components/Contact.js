769fe06c2560655b11f3c1e37091765785e751c3
Absorberend <102243063+Absorberend@users.noreply.github.com>
2022-10-31 12:06:08 +0100

    added contactform functionality

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,13 +1,30 @@
-import React, {useState} from "react";
+import React, {useState, useRef} from "react";
 import "./Contact.css";
 import mailIcon from "../assets/mail.svg";
 import linkedInIcon from "../assets/brand-icons/brand-linkedin.svg";
 import githubIcon from "../assets/brand-icons/brand-github.svg";
+import emailjs from '@emailjs/browser';
 
 export default function Contact({theme}) {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [message, setMessage] = useState("");
+  const form = useRef();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_93v9yjk', 'template_uri8pss', form.current, 'h2jh2zTHRL5oz1Wxl')
+    .then((result) => {
+        console.log(result.text);
+    }, (error) => {
+        console.log(error.text);
+    });
+
+    setName("");
+    setEmail("");
+    setMessage("");
+  }
 
   return (
     <section className="contact__section" id="Contact">
@@ -16,6 +33,8 @@ export default function Contact({theme}) {
       <form 
         className={`contact__form contact__form__${theme}`} 
         name="contact__form"
+        onSubmit={(e) => handleSubmit(e)}
+        ref={form}
       >
         <div className="contact__social__media">
           <a href="/" >
@@ -33,7 +52,7 @@ export default function Contact({theme}) {
           <input 
             className={`input__${theme}__theme`}
             type="text" 
-            name="contact__form" 
+            name="user_name" 
             id="contact__name" 
             size="25" 
             placeholder="Name" 
@@ -48,7 +67,7 @@ export default function Contact({theme}) {
           <input 
             className={`input__${theme}__theme`}
             type="email" 
-            name="contact__form" 
+            name="user_email" 
             id="contact__email" 
             size="25" 
             placeholder="Email address" 
@@ -64,7 +83,7 @@ export default function Contact({theme}) {
             className={`input__${theme}__theme`}
             rows="7" 
             cols="42"
-            name="contact__form" 
+            name="message" 
             id="contact__message"
             placeholder="Type your message here."
             value={message} 
