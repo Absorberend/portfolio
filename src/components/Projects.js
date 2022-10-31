681ec72a6fbb9440ecf5ffb6a5f55ceb998c7bf5
Absorberend <102243063+Absorberend@users.noreply.github.com>
2022-10-31 15:09:24 +0100

    edited portfolio text

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -33,7 +33,7 @@ export default function Projects({theme, matches, onProjectModalClick}) {
           <p>
             With this project you can search 200+ countries and view detailed information on each of the country's pages.
             The fetched data is shown to the user on-screen by making use of sereval map and/or filter functions.
-            This project is built with React and uses React Router as routing. Several custom Hooks are used to fetch data or close modals etc.
+            This project is built with React and uses React Router as routing. Several custom hooks are used to fetch data or close a dropdown menu.
           </p>
           <div className="project__info__text__links">
             <a 
