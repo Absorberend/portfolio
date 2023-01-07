1402853746145090853458ebc3a01a04156aab1d
Absorberend <102243063+Absorberend@users.noreply.github.com>
2023-01-07 13:24:00 +0100

    edited about text

--- src/components/About.js
+++ src/components/About.js
@@ -7,10 +7,9 @@ export default function About({theme}) {
     <section className={`about__section about__section__${theme}`} id="About">
       <h2 className="about__header"><span className="about__wave">👋</span> Hi, I'm Berend.</h2>
       <p className="about__text">
-        I'm a frontend developer from the Netherlands. After experiencing the endless possibilities within the crypto space I became very passionate about web development.
-        Creating webpages that truly come to life and provide value to users is just astonishing to me. 
-        I decided to give up my stable career in accounting and take on this great web development adventure. 
-        I'm very eager to learn and provide as much value as I possibly can to your project or company.
+        I'm a web developer from the Netherlands. After working as a bookkeeper and dealing with business processes I became very passionate about development. 
+        Being able to built and come up with solutions to complex problems is something I really enjoy doing. 
+        I'm very eager to learn and provide as much value as I possibly can to your project or company. 
       </p>
       <p className="about__text"> These are the technologies I've got experience with so far:</p>
       <Skills theme={theme} />
