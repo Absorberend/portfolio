2bfc0a163a3655e8f7dc3657db9e1816ded296ff
Absorberend <102243063+Absorberend@users.noreply.github.com>
2023-01-09 09:30:09 +0100

    fix typo

--- src/components/About.js
+++ src/components/About.js
@@ -8,7 +8,7 @@ export default function About({theme}) {
       <h2 className="about__header"><span className="about__wave">👋</span> Hi, I'm Berend.</h2>
       <p className="about__text">
         I'm a web developer from the Netherlands. After working as a bookkeeper and dealing with business processes I became very passionate about development. 
-        Being able to built and come up with solutions to complex problems is something I really enjoy doing. 
+        Being able to build and come up with solutions to complex problems is something I really enjoy doing. 
         I'm very eager to learn and provide as much value as I possibly can to your project or company. 
       </p>
       <p className="about__text"> These are the technologies I've got experience with so far:</p>
